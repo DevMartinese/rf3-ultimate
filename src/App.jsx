4f42d15be45d8a1e057602485e4c87dfafbd6c90
DevMartinese <devmartinese@gmail.com>
2024-01-24 12:20:43 -0300

    add: learn about 3D objects, Geometry, Material and Mesh

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import * as THREE from 'three';
 
 function App() {
   return (
     <Canvas camera={{ position: [3, 3, 3] }}>
-      <OrbitControls />
-      <mesh>
+      <mesh position-x={-0.6}>
         <boxGeometry />
-        <meshNormalMaterial />
+        <meshStandardMaterial color={'#ff0000'} side={THREE.FrontSide} />
       </mesh>
+      <mesh position-x={0.6} position-z={-1}>
+        <boxGeometry />
+        <meshStandardMaterial color='hotpink' side={THREE.DoubleSide} />
+      </mesh>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 3]} intensity={1} />
+      <directionalLight position={[0, 3, 3]} intensity={0.5} />
     </Canvas>
   )
 }
