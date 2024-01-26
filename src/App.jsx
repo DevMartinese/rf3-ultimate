9c00dbc60a9b85785d836f6f7078c3bcfd7bd804
DevMartinese <devmartinese@gmail.com>
2024-01-26 11:38:39 -0300

    add: learn camera lesson, and adding components/models

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
 import { Canvas } from '@react-three/fiber'
-import * as THREE from 'three';
+import { City } from './components/City';
+import { Environment, OrbitControls, OrthographicCamera, PerspectiveCamera } from '@react-three/drei';
 
 function App() {
   return (
-    <Canvas camera={{ position: [0, 3, 8] }}>
-      {/*Lighting*/}
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 3, 5]} intensity={0.5} />
-
-      {/*Objects*/}
-      <group position={[-2, -2, 0]} scale={2} rotation-y={Math.PI / 4}>
-        <mesh position-x={-1}>
-          <boxGeometry />
-          <meshStandardMaterial color='red' />
-        </mesh>
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial color='blue' />
-        </mesh>
-        <mesh position-x={1}>
-          <boxGeometry />
-          <meshStandardMaterial color='green' />
-        </mesh>
-      </group>
+    <Canvas>
+      <OrthographicCamera
+        position={[1, 1, 1]}
+        makeDefault
+        top={2}
+        bottom={-2}
+        left={-2 * (window.innerWidth /window.innerHeight)}
+        right={2 * (window.innerWidth /window.innerHeight)}
+        near={-5}
+      />
+      <OrbitControls />
+      <City />
+      <Environment preset='city' />
     </Canvas>
   )
 }
