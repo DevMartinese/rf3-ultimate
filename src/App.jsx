e51cf82ae8dec678f9e0e50591b4f2712bfe9b04
DevMartinese <devmartinese@gmail.com>
2024-03-31 22:04:17 -0300

    add r3f hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,27 @@
-import { memo, useEffect, useMemo, useRef, useState, useCallback } from 'react';
 import { Environment, OrbitControls } from '@react-three/drei';
-import { ThemeProvider, useTheme } from './hooks/useTheme';
 import ContactShadow from './experiences/ContactShadow';
-import { Canvas } from '@react-three/fiber';
-import { button, useControls } from 'leva';
+import { Canvas, useThree } from '@react-three/fiber';
 
-const UI = () => {
-  const { setColor } = useTheme();
-
-  useControls({
-    changeColorToRed: button(() => setColor('red')),
-    changeColorToGreen: button(() => setColor('green')),
-    changeColorToBlue: button(() => setColor('blue'))
-  })
-}
-
-const Cube = memo((props) => {
-  console.log('Cube Rendered')
-  const { color } = useTheme();
-  const refCube = useRef();
-
-  const material = useMemo(
-    () => <meshStandardMaterial color={color} />,
-    [color]
-  )
-
-  useControls({
-    cubeRotation: button(() => refCube.current.rotation.y += Math.PI / 4)
-  })
-
-  useEffect(() => {
-    const colorPosition = {
-      white: [0, 0, 0],
-      red: [-1, 0, 0],
-      green: [1, 0, 0],
-      blue: [0, 1, 0]
-    };
-
-    const position = colorPosition[color];
-
-    refCube.current.position.set(...position);
-
-    const interval = setInterval(() => {
-      refCube.current.rotation.y += Math.PI / 4
-    }, 1000);
-
-    return () => {
-      clearInterval(interval)
-    }
-  }, [color])
+const Cube = (props) => {
+  const { camera } = useThree();
 
   return (
-    <mesh {...props} ref={refCube} >
+    <mesh {...props} >
       <boxGeometry />
-      {material}
+      <meshStandardMaterial />
     </mesh>
   )
-});
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-  const onCubeClicked = useCallback(() => {
-    console.log(`Cube clicked ${count} time${count > 1 ? 's' : ''}`);
-    setCount((prev) => prev + 1);
-  }, [count]);
-
   return (
     <>
-      <ThemeProvider>
-        <UI />
-        <Canvas camera={{ position: [0, 2, 6], fov: 42 }}>
-          <OrbitControls />
-          <Cube rotation-y={Math.PI / 4} onClick={onCubeClicked} />
-          <ContactShadow />
-          <Environment preset='city' />
-        </Canvas>
-      </ThemeProvider>
+      <Canvas camera={{ position: [0, 2, 6], fov: 42 }}>
+        <OrbitControls />
+        <Cube rotation-y={Math.PI / 4} />
+        <ContactShadow />
+        <Environment preset='city' />
+      </Canvas>
     </>
   )
 }
