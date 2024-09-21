8a6bd07ce45bd584b8dc769de806d716d1d0483c
DevMartinese <devmartinese@gmail.com>
2024-09-21 10:08:18 -0300

    add: https config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], 
+  server: {
+    https: false, // Cambia el puerto a otro número
+  },
 })
